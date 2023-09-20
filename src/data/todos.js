e788199a3d385acac6789846b0a1ac4ccb85a0c5
LBF38 <contact.lbf38@gmail.com>
2023-09-20 16:03:05 +0200

    feat(server): :sparkles: add new todos + update server routes
    add todos elements
    add GET route in the server

--- src/data/todos.js
+++ src/data/todos.js
@@ -2,13 +2,25 @@ const todos = [
     {
         id: 1,
         title: "Creating a web server",
-        description: "Adding a server object",
+        description: "Adding a server object and listener",
         completed: true
     },
     {
         id: 2,
         title: "Going to the gym",
-        description: "Lifting weights",
+        description: "Lifting weights and doing push-ups",
+        completed: false
+    },
+    {
+        id: 3,
+        title: "Playing video games",
+        description: "Finishing The Legend of Zelda walkthrough for the first time",
+        completed: false
+    },
+    {
+        id: 4,
+        title: "Taking a shower",
+        description: "Applying shampoo and conditioner and using a bath sponge",
         completed: false
     }
 ]
--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 const http = require('http')
 const todos = require('./data/todos')
 const PORT = process.env.PORT || 5000
+const getRequestData = require('./utils')
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
-    })
-    response.end("Hello World")
+const server = http.createServer(async (request, response) => {
+    if (request.url === '/api/todos' && request.method === 'GET') {
+        response.writeHead(200, {
+            'content-type': 'application/json'
+        })
+        response.end(JSON.stringify(todos))
+    } else if (request.url === "/api/todos" && request.method === "POST") {
+        const body = await getRequestData(request)
+        const task = JSON.parse(body)
+        todos.push(task)
+        response.writeHead(201, {
+            "Content-Type": "application/json"
+        })
+        response.end(JSON.stringify(todos))
+        return;
+    }
 })
 
 server.listen(PORT, () => {
