5cc5e86fb424e422551a450584a502e9d6660841
LBF38 <contact.lbf38@gmail.com>
2023-10-18 15:26:10 +0200

    feat(activities): :sparkles: add simple GraphQL API - bloggingapp (lab4)
    add a simple GraphQL schema
    add corresponding resolvers + fake db

--- lab4/bloggingapp/index.js
+++ lab4/bloggingapp/index.js
@@ -1,8 +1,14 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-//We need to define our schema and resolvers before executing the server.
-const server = new ApolloServer({});
+import { typeDefs } from './src/schema.js';
+import { resolvers } from './src/resolvers.js';
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
 });
-console.log('Server ready at: ${url}');
+console.log(`Server ready at: ${url}`);
